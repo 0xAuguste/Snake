2d761138e0cb70d871abbe8513980cef31715a18
gusbrown05 <gusinator9@gmail.com>
2017-05-11 11:49:05 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,47 +1,48 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-public class Board extends JPanel{
-	public Board(){
-		JFrame board = new JFrame();
-		board.setSize(1200, 822);
-		board.add(this);
-		board.setVisible(true);
-		board.setBackground(Color.white);
-	}	
-	public static void main(String[] args){
-		new Board();
-	}
-	private int x = 30;
-    private int y = 30;
+public class Board extends JPanel // implements ActionListener
+{
+    public Board(){
+        JFrame board = new JFrame();
+        board.setSize(1200, 822);
+        board.add(this);
+        board.setVisible(true);
+    }   
+
+    private int x = 20;
+    private int y = 20;
     public void paintComponent(Graphics g){
         g.setColor(Color.white);
         g.fillRect(0,0,1200,800);
         g.setColor(Color.blue);
-        g.fillRect(0, 0, 30, 800);
-        g.fillRect(0, 0, 1200, 30);
-        g.fillRect(1170, 0, 30, 800);
-        g.fillRect(0, 770, 1200, 30);
+        g.fillRect(0, 0, 20, 800);
+        g.fillRect(0, 0, 1200, 20);
+        g.fillRect(1180, 0, 20, 800);
+        g.fillRect(0, 780, 1200, 20);
         g.setColor(Color.black);
-        g.fillRect(x,y,30,30);
+        g.fillRect(x,y,20,20);
     }
 
     public void moveRight(){
-        x+=30;
+        x+=20;
         repaint();
     }
     
     public void moveLeft(){
-        x-=30;
+        x-=20;
         repaint();
     }
     
     public void moveDown(){
-        y+=30;
+        y+=20;
         repaint();
     }
     
     public void moveUp(){
-        y-=30;
+        y-=20;
         repaint();
     }
+    
+    
 }
