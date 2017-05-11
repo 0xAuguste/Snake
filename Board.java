d37bfc42b5e3802fb6b53b48859804bff3c0c4ef
gusbrown05 <gusinator9@gmail.com>
2017-05-11 13:42:21 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,17 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Board extends JPanel // implements ActionListener
+public class Board extends JPanel implements KeyListener
 {
     public Board(){
+        addKeyListener(this);
+        setFocusable(true);
         JFrame board = new JFrame();
         board.setSize(1200, 822);
         board.add(this);
         board.setVisible(true);
     }   
-
+    public static void main(String[] args){
+    	new Board();
+    }
     private int x = 20;
     private int y = 20;
+    private int currentDirection;
     public void paintComponent(Graphics g){
         g.setColor(Color.white);
         g.fillRect(0,0,1200,800);
@@ -26,23 +31,54 @@ public class Board extends JPanel // implements ActionListener
 
     public void moveRight(){
         x+=20;
-        repaint();
     }
     
     public void moveLeft(){
         x-=20;
-        repaint();
     }
     
     public void moveDown(){
         y+=20;
-        repaint();
     }
     
     public void moveUp(){
         y-=20;
+    }
+    
+    public void keyPressed(KeyEvent e){
+        if(currentDirection == 0){
+            currentDirection = e.getKeyCode();
+        }
+        else if(currentDirection == KeyEvent.VK_LEFT && e.getKeyCode() != KeyEvent.VK_RIGHT){
+            currentDirection = e.getKeyCode();
+        }
+        else if(currentDirection == KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_LEFT){
+            currentDirection = e.getKeyCode();
+        }
+        else if(currentDirection == KeyEvent.VK_DOWN && e.getKeyCode() != KeyEvent.VK_UP){
+            currentDirection = e.getKeyCode();
+        }
+        else if(currentDirection == KeyEvent.VK_UP && e.getKeyCode() != KeyEvent.VK_DOWN){
+            currentDirection = e.getKeyCode();
+        }
+        move();
+    }
+    
+    public void keyReleased(KeyEvent e){}
+    
+    public void keyTyped(KeyEvent e){}
+    
+    public void move(){
+        if(currentDirection == KeyEvent.VK_LEFT)moveLeft();
+        else if(currentDirection == KeyEvent.VK_RIGHT)moveRight();
+        else if(currentDirection == KeyEvent.VK_DOWN)moveDown();
+        else if(currentDirection == KeyEvent.VK_UP)moveUp();
         repaint();
     }
     
-    
+    public void game(){
+        while(true){
+            move();
+        }
+    }
 }
